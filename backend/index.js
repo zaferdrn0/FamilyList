822e6349c560d00f39c4d01ded77adb611278d15
zaferdrn0 <zaferdrn0@gmail.com>
2024-04-10 01:24:25 +0300

    mongodb packages installed and database connection established

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 4000
+import express from 'express';
+import mongoose from 'mongoose';
+import { User } from './models/user.js';
+const app = express();
+const port = 4000;
 
-app.get('/api/test', (req, res) => {
-    const data = {
-        data:"hello world"
-    }
-  res.send(data)
-})
+app.use(express.json()); // To process JSON requests
+
+// Mongodb database connection
+mongoose.connect('mongodb://localhost:27017/FamilyList')
+  .then(() => console.log('Successfully connect Mongo DB'))
+  .catch(err => console.error('MongoDB connect error:', err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server ${port} port running...`);
+});
