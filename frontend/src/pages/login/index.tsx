a8eab1b9142d2e178d1539a715da5793871a108a
zaferdrn0 <zaferdrn0@gmail.com>
2024-04-10 00:41:54 +0300

    added fetch api functions and checked whether the communication with the backend was successful

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { fetchBackendGET } from '@/utils/backendFetch'
+import React, { useEffect } from 'react'
 
 const Login = () => {
+
+    const fetchData =  async ()  =>{
+        const test = await fetchBackendGET("/test")
+        if(test.ok){
+            
+           const data = await test.json()
+           console.log(data)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+      },[]);
+
   return (
     <div>Login</div>
   )
